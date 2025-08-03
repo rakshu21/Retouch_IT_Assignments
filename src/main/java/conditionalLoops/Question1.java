1901a3b7c3f1fec2e749dd18eeae132e28b59ee3
rakshu21 <165006247+rakshu21@users.noreply.github.com>
2025-08-03 22:45:14 +0530

    Update Question1.java

--- src/main/java/conditionalLoops/Question1.java
+++ src/main/java/conditionalLoops/Question1.java
@@ -4,7 +4,7 @@ public class Question1 {
     public static void main(String[] args) {
         //initlize and declare the variable and value
         int score =48;
-        //•	If the score is 50 or more, print "Passed" or "Failed"
+        //If the score is 50 or more, print "Passed" or "Failed"
         if (score >= 50) {
             System.out.println("Passed");
         } else {
